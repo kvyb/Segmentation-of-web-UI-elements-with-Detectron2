b2d9d147991874cb0a5ef7a763372e51827dd3ac
kvyb <vybiral.kristian@gmail.com>
2020-07-24 00:21:42 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,91 @@
+# You may need to restart your runtime prior to this, to let your installation take effect
+# Some basic setup:
+# Setup detectron2 logger
+import detectron2
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+# import some common libraries
+import numpy as np
+from cv2 import cv2
+import random
+
+# import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog
+from detectron2.data.catalog import DatasetCatalog
+
+from detectron2.data.datasets import register_coco_instances
+register_coco_instances("my_dataset_train", {}, "content/train/_annotations.coco.json", "content/train")
+register_coco_instances("my_dataset_val", {}, "content/valid/_annotations.coco.json", "content/valid")
+register_coco_instances("my_dataset_test", {}, "content/test/_annotations.coco.json", "content/test")
+
+#visualize training data
+my_dataset_train_metadata = MetadataCatalog.get("my_dataset_train")
+dataset_dicts = DatasetCatalog.get("my_dataset_train")
+
+import random
+from detectron2.utils.visualizer import Visualizer
+
+for d in random.sample(dataset_dicts, 3):
+    img = cv2.imread(d["file_name"])
+    visualizer = Visualizer(img[:, :, ::-1], metadata=my_dataset_train_metadata, scale=0.5)
+    vis = visualizer.draw_dataset_dict(d)
+    cv2.imshow('preview',vis.get_image()[:, :, ::-1])
+    cv2.waitKey(1000)
+
+#We are importing our own Trainer Module here to use the COCO validation evaluation 
+# during training. Otherwise no validation eval occurs.
+
+from detectron2.engine import DefaultTrainer
+from detectron2.evaluation import COCOEvaluator
+
+class CocoTrainer(DefaultTrainer):
+
+  @classmethod
+  def build_evaluator(cls, cfg, dataset_name, output_folder=None):
+
+    if output_folder is None:
+        os.makedirs("coco_eval", exist_ok=True)
+        output_folder = "coco_eval"
+
+    return COCOEvaluator(dataset_name, cfg, False, output_folder)
+
+#from .detectron2.tools.train_net import Trainer
+#from detectron2.engine import DefaultTrainer
+from detectron2.config import get_cfg
+#from detectron2.evaluation.coco_evaluation import COCOEvaluator
+import os
+
+cfg = get_cfg()
+cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"))
+cfg.DATASETS.TRAIN = ("my_dataset_train",)
+cfg.DATASETS.TEST = ("my_dataset_val",)
+
+cfg.DATALOADER.NUM_WORKERS = 2
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
+cfg.SOLVER.IMS_PER_BATCH = 2
+cfg.SOLVER.BASE_LR = 0.001
+
+
+cfg.SOLVER.WARMUP_ITERS = 1000
+cfg.SOLVER.MAX_ITER = 1500 #adjust up if val mAP is still rising, adjust down if overfit
+cfg.SOLVER.STEPS = (1000, 1500)
+cfg.SOLVER.GAMMA = 0.05
+
+
+
+
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 32
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 31 #your number of classes + 1
+
+cfg.TEST.EVAL_PERIOD = 50
+
+
+os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+trainer = CocoTrainer(cfg)
+trainer.resume_or_load(resume=False)
+trainer.train()
