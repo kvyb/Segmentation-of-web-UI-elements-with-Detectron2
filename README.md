499cf32725f6847ec5a3f79d216acdb34cb002d5
kvyb <vybiral.kristian@gmail.com>
2020-07-25 18:43:54 +0300

    Inference, debugging, readme

---
+++ README.md
@@ -0,0 +1,86 @@
+# Readme
+
+## Install Cuda and cuDNN
+
+Below is the setup process. Assuming you have Ubuntu 20.04 installed with an Nvidia GPU available.
+
+To make pytorch work with GPU, it is necessary to install CUDA and cuDNN. If working from Ubuntu 20.04 the [following guide](https://askubuntu.com/questions/1230645/when-is-cuda-gonna-be-released-for-ubuntu-20-04) is suitable.
+  
+
+**After installing both CUDA and cuDNN check CUDA and cuDNN are version 10.1 and 7.6.5 respectively:**
+
+**CUDA:**
+
+    $ nvcc -V
+
+**cuDNN:**
+
+get where cuda installed:
+
+    $ whereis cuda
+then navigate to that folder's /include folder and: 
+
+    $ cat cudnn.h | grep CUDNN_MAJOR -A 2
+
+  
+  
+
+## Activate conda venv with tensorflow-gpu==2.2.0
+
+ensure that conda is initialised with Python 3.8.3 (set it within conda environment creation).
+
+See [cheatsheet](https://docs.conda.io/projects/conda/en/4.6.0/_downloads/52a95608c49671267e40c689e0bc00ca/conda-cheatsheet.pdf) !
+  
+
+To install correct pytorch and torchvision do this with conda activated environment:
+
+    $ conda install pytorch torchvision cudatoolkit=10.1 -c pytorch
+
+Check that GPU is available and working. In virtual environment `$ python`:
+
+    import torch
+    torch.cuda.current_device()
+    torch.cuda.get_device_name(0)
+
+Then install some Detectron2 dependencies:
+
+    $ pip install cython pyyaml==5.1
+    $ pip install pycocotools>=2.0.1
+Then install Detectron2:
+
+    $ pip install detectron2==0.1.3 -f https://dl.fbaipublicfiles.com/detectron2/wheels/cu101/torch1.5/index.html
+
+See also Detectron2 [installation guide](https://github.com/facebookresearch/detectron2/blob/master/INSTALL.md). 
+
+  
+
+**Note:**
+Ensure that the eval set contains at least 1 instance of all classes that can be encountered in the training dataset:
+
+    cfg.DATASETS.TEST = ("my_dataset_val",
+
+
+Otherwise the validation step will fail at EVAL_PERIOD.
+
+Also make sure that your data is marked according to COCO standard. A good tool to use is the [VGG annotator](http://www.robots.ox.ac.uk/~vgg/software/via/via_demo.html) (export annotations to COCO json).
+  
+**Run from conda virtual env:**
+
+    $ python main.py
+
+  
+**Output:**
+to graphically visualise and get data about model performance, after training completes, run:
+
+    $ tensorboard --logdir output
+
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -38,7 +38,7 @@ for d in random.sample(dataset_dicts, 3):
     cv2.waitKey(1000)
 
 #We are importing our own Trainer Module here to use the COCO validation evaluation 
-# during training. Otherwise no validation eval occurs.
+#during training. Otherwise no validation eval occurs.
 
 from detectron2.engine import DefaultTrainer
 from detectron2.evaluation import COCOEvaluator
@@ -60,6 +60,7 @@ from detectron2.config import get_cfg
 #from detectron2.evaluation.coco_evaluation import COCOEvaluator
 import os
 
+# Pick a base model that your GPU can handle here: https://github.com/facebookresearch/detectron2/blob/master/MODEL_ZOO.md
 cfg = get_cfg()
 cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"))
 cfg.DATASETS.TRAIN = ("my_dataset_train",)
@@ -82,10 +83,57 @@ cfg.SOLVER.GAMMA = 0.05
 cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 32
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = 31 #your number of classes + 1
 
-cfg.TEST.EVAL_PERIOD = 50
+cfg.TEST.EVAL_PERIOD = 50 #Adjust this to set at what interval the model performance should be
+                          #evaluated. Currently adjusted for eval debug.
 
 
 os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
 trainer = CocoTrainer(cfg)
-trainer.resume_or_load(resume=False)
+
+###
+# Set (resume=False) if running for the first time!
+###
+trainer.resume_or_load(resume=True)
+
 trainer.train()
+
+#Should now have "model_final.pth" weights file.
+
+from detectron2.data import DatasetCatalog, MetadataCatalog, build_detection_test_loader
+from detectron2.evaluation import COCOEvaluator, inference_on_dataset
+# Run evaluation on the test set. Set the test and val sets appropriately..
+
+cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.85
+predictor = DefaultPredictor(cfg)
+evaluator = COCOEvaluator("my_dataset_train", cfg, False, output_dir="./output/")
+val_loader = build_detection_test_loader(cfg, "my_dataset_train")
+inference_on_dataset(trainer.model, val_loader, evaluator)
+
+print("Evaluation on test set finished")
+
+"""# Inference with Detectron2 Saved Weights"""
+
+cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
+cfg.DATASETS.TEST = ("my_dataset_test", )
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set the testing threshold for this model
+predictor = DefaultPredictor(cfg)
+test_metadata = MetadataCatalog.get("my_dataset_test")
+from detectron2.utils.visualizer import ColorMode
+import glob
+
+print("Starting to output inference results. Predictions below 0.7 score ignored.")
+#Set the correct format. If using JPG files set *.JPG instead of *.PNG !
+for imageName in glob.glob('content/test/*.PNG'):
+
+    print("{}".format(imageName))
+
+    im = cv2.imread(imageName)
+    outputs = predictor(im)
+    v = Visualizer(im[:, :, ::-1],
+                    metadata=my_dataset_train_metadata, 
+                    scale=0.5
+                    )
+    out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+    cv2.imshow('preview2',out.get_image()[:, :, ::-1])
+    cv2.waitKey(10000)
